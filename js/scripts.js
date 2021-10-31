3a20ef369e31604bf2a08c73b3cfa92867552a2c
matt29880 <mathieu@ronvel.com>
2021-10-31 15:53:33 +0100

    progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,3 @@
-/*!
-* Start Bootstrap - Clean Blog v6.0.5 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
-window.addEventListener('DOMContentLoaded', () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
-        const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
-            } else {
-                mainNav.classList.remove('is-visible', 'is-fixed');
-            }
-        } else {
-            // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
-            }
-        }
-        scrollPos = currentTop;
-    });
-})
-
 let totalImages = 15;
 var imageCounter = 1;
 
@@ -38,7 +9,7 @@ setInterval(function(){ changeImage(); }, 5000);
 
 function changeImage()
 {
-  document.getElementById("header").style.backgroundImage = "url(img/us/" + (imageCounter % (totalImages - 1)) + ".jpg)";
+  document.getElementById("splash-container").style.backgroundImage = "url(../img/us/" + (imageCounter % (totalImages - 1)) + ".jpg)";
   imageCounter++;
 }
 
